866876b9dc96cd829182b058da30f010f511243b
1 <1.com>
2022-10-13 10:32:27 +0800

    “数据类型所占内存大小”

---
+++ Project3/Project3/源.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+     
+    printf("char:%d\n", sizeof(char));
+    printf("short:%d\n", sizeof(short));
+    printf("long:%d\n", sizeof(long));
+    return 0;
+}
