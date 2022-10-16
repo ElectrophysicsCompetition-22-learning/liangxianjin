014c2bd1fb9c80ddac5429fdba785ed8a9747932
1 <1.com>
2022-10-16 21:15:10 +0800

    “ax+b=0”

---
+++ Project6/Project6/源.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+         float a, b;
+        scanf_s("%f %f", &a, &b);
+        float x = (-b) / a;
+        printf("%f", x); 
+
+
+
+                   return 0;
+          }
